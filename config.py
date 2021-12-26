bab0aeeacca0a6877b5bc0f857a5f49d8f723496
Anshul Garg <garganshul553@gmail.com>
2021-12-26 22:30:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
-USE_GROUP = os.environ.get("USE_GROUP", None)
+USE_GROUP = int(os.environ.get("USE_GROUP", None))
 #Set true if you want Disable your Channel Posts Share button
 if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
     DISABLE_CHANNEL_BUTTON = True
